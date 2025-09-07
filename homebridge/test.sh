c6fbdf3561342a5937951bf6a3d62b9172876aaf
Boris Rybalkin <ribalkin@gmail.com>
2025-09-07 09:09:40 +0100

    fix sh

--- homebridge/test.sh
+++ homebridge/test.sh
@@ -4,5 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd ${DIR}
 
 BUILD_DIR=${DIR}/../build/snap/homebridge
-${BUILD_DIR}/bin/node --version
-${BUILD_DIR}/usr/local/bin/ffmpeg -version
+${BUILD_DIR}/bin/node.sh --version
+${BUILD_DIR}/usr/local/bin/ffmpeg.sh -version
