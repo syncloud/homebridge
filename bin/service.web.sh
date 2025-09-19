639fe012f2ee27192baa656a5af5c0262b8f1392
Boris Rybalkin <ribalkin@gmail.com>
2025-09-19 22:42:50 +0100

    fix node

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -1,10 +1,9 @@
 #!/bin/bash -e
 /bin/rm -f $SNAP_COMMON/web.socket
 #export UIX_CUSTOM_PLUGIN_PATH=$SNAP/homebridge/opt/homebridge
-exec $SNAP/homebridge/opt/homebridge/bin/node \
+exec $SNAP/homebridge/bin/node.sh \
   $SNAP/homebridge/opt/homebridge/lib/node_modules/homebridge-config-ui-x/dist/bin/hb-service.js \
   run -I \
   -U $SNAP_DATA/storage \
   -P $SNAP_DATA/storage/node_modules \
   --strict-plugin-resolution "$@"
-
