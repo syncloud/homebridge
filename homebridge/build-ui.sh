6947a249dd579f42ad8b527d0cfce02453804d03
Boris Rybalkin <ribalkin@gmail.com>
2025-09-19 07:37:48 +0100

    no fork

--- homebridge/build-ui.sh
+++ homebridge/build-ui.sh
@@ -20,16 +20,13 @@ npm i
 cd ..
 npm run build
 
-rm -rf $UI_DIR/dist
-mv dist $UI_DIR
-rm -rf $UI_DIR/node_modules
-mv node_modules $UI_DIR
-rm -rf $UI_DIR/public
-mv public $UI_DIR
-rm -rf $UI_DIR/scripts
-mv scripts $UI_DIR
-cp package.json $UI_DIR
-#cp config.schema.json $UI_DIR
-
-
+#rm -rf $UI_DIR/dist
+#mv dist $UI_DIR
+#rm -rf $UI_DIR/node_modules
+#mv node_modules $UI_DIR
+#rm -rf $UI_DIR/public
+#mv public $UI_DIR
+#rm -rf $UI_DIR/scripts
+#mv scripts $UI_DIR
+#cp package.json $UI_DIR
 
