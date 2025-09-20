af5cb0ce08e9f785d520564e698694ae333791da
Boris Rybalkin <ribalkin@gmail.com>
2025-09-20 14:03:14 +0100

    try ci and node modules

--- homebridge/build-ui.sh
+++ homebridge/build-ui.sh
@@ -14,9 +14,9 @@ sed -i '/async findHomebridgePath() {/a this.homebridgeModulePath = "\/snap\/hom
 wget https://github.com/cyberb/homebridge-config-ui-x/archive/refs/heads/v$VERSION.tar.gz
 tar xf v$VERSION.tar.gz
 cd homebridge-config-ui-x-$VERSION
-npm i
+npm ci
 cd ui
-npm i
+npm ci
 cd ..
 npm run build
 
@@ -24,11 +24,13 @@ ls $UI_DIR/dist
 rm -rf $UI_DIR/dist
 ls dist
 mv dist $UI_DIR
-#rm -rf $UI_DIR/node_modules
-#mv node_modules $UI_DIR
+rm -rf $UI_DIR/node_modules
+mv node_modules $UI_DIR
 #rm -rf $UI_DIR/public
 #mv public $UI_DIR
 #rm -rf $UI_DIR/scripts
 #mv scripts $UI_DIR
 #cp package.json $UI_DIR
+
+
 
