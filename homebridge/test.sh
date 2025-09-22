23e6c3325ea2153eb8027fa9ae9dc2c2784d021b
Boris Rybalkin <ribalkin@gmail.com>
2025-09-22 22:24:39 +0100

    no test

--- homebridge/test.sh
+++ homebridge/test.sh
@@ -6,4 +6,4 @@ cd ${DIR}
 BUILD_DIR=${DIR}/../build/snap
 ${BUILD_DIR}/homebridge/bin/node.sh --version
 ${BUILD_DIR}/homebridge/bin/ffmpeg.sh -version
-${BUILD_DIR}/backend/node_modules/homebridge/bin/homebridge --version
+#${BUILD_DIR}/backend/node_modules/homebridge/bin/homebridge --version
