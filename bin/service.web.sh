e45ec136638178e86ff21795c31aef5282db4a44
Boris Rybalkin <ribalkin@gmail.com>
2025-09-27 09:33:16 +0100

    homebridge version

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 /bin/rm -f $SNAP_COMMON/web.socket
 export UIX_CUSTOM_NODEJS_PATH=$SNAP/homebridge/bin/node.sh
+export NODE_PATH=$SNAP/backend/node_modules
 $SNAP/homebridge/bin/node.sh \
   $SNAP/homebridge/opt/homebridge/lib/node_modules/homebridge-config-ui-x/dist/bin/hb-service.js \
   run -I \
