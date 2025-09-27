f4eb03861e820dc952eb14b8c81fdf0d7c85cc90
Boris Rybalkin <ribalkin@gmail.com>
2025-09-27 09:47:58 +0100

    disable sudo

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -2,6 +2,7 @@
 /bin/rm -f $SNAP_COMMON/web.socket
 export UIX_CUSTOM_NODEJS_PATH=$SNAP/homebridge/bin/node.sh
 export NODE_PATH=$SNAP/backend/node_modules
+exrtpo HOMEBRIDGE_CONFIG_UI_SUDO=0
 $SNAP/homebridge/bin/node.sh \
   $SNAP/homebridge/opt/homebridge/lib/node_modules/homebridge-config-ui-x/dist/bin/hb-service.js \
   run -I \
