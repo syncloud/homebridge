9a8add68a48403f7e1eac3206ae5e94677ea60e9
Boris Rybalkin <ribalkin@gmail.com>
2025-09-28 16:41:39 +0100

    plugin install

--- test/ui.py
+++ test/ui.py
@@ -63,5 +63,8 @@ def test_plugins(selenium, device_user, device_password):
     search.send_keys(Keys.RETURN)
     selenium.find_by_xpath("//div[contains(.,'Tplink Smarthome')]")
     selenium.screenshot('plugins-tplink')
+    selenium.find_by_xpath("//i[contains(@class,'fa-arrow-alt-circle-down')]")
+    selenium.find_by_xpath("//div[contains(.,'latest')]")
+    selenium.screenshot('plugins-tplink-install')
 
 
