7939cef951ad793a4283f0044edc05ee691f1d30
Boris Rybalkin <ribalkin@gmail.com>
2025-09-29 09:00:18 +0100

    child path

--- homebridge/npm.sh
+++ homebridge/npm.sh
@@ -13,3 +13,5 @@ npm i homebridge@$VERSION
 sed -i 's#/usr/bin/env node#/snap/homebridge/current/homebridge/bin/node.sh#g' $BUILD_DIR/node_modules/homebridge/bin/homebridge
 
 head $BUILD_DIR/node_modules/homebridge/bin/homebridge
+
+sed -i 's#silent = true#silent = true, execPath: process.env.UIX_CUSTOM_NODEJS_PATH#g' $BUILD_DIR/node_modules/homebridge/lib/childBridgeService.js
