cbc93a6392440d06ecda0b0e55716435ebf11523
Boris Rybalkin <ribalkin@gmail.com>
2025-09-29 21:42:29 +0100

    UIX_CUSTOM_PLUGIN_PATH

--- bin/service.web.sh
+++ bin/service.web.sh
@@ -4,9 +4,11 @@ export UIX_CUSTOM_NODEJS_PATH=$SNAP/homebridge/bin/node.sh
 export NODE_PATH=$SNAP/backend/node_modules
 export HOMEBRIDGE_CONFIG_UI_SUDO=0
 export PATH=$PATH:$SNAP/bin
+export UIX_CUSTOM_PLUGIN_PATH=$SNAP_DATA/storage/node_modules
+
 $SNAP/homebridge/bin/node.sh \
   $SNAP/homebridge/opt/homebridge/lib/node_modules/homebridge-config-ui-x/dist/bin/hb-service.js \
   run -I \
   -U $SNAP_DATA/storage \
-  -P $SNAP_DATA/storage/node_modules \
+  -P $UIX_CUSTOM_PLUGIN_PATH \
   --strict-plugin-resolution "$@"
